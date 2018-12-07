74ee226a0e487d4cc5252340e250c7e1a502226f
Lukas Rychtecky <lukas.rychtecky@gmail.com>
2018-12-07 11:22:27 +0100

    0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject garm "0.0.0"
+(defproject garm "0.1.0"
   :description "Garm is a library that decorates Clojure Specs for better understanding"
   :url "https://github.com/druids/garm"
   :license {:name "MIT License"
