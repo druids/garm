554cc24832cb9f0c46c2fcf3bc96ad2cd919de6f
Lukas Rychtecky <lukas.rychtecky@gmail.com>
2019-02-26 15:22:05 +0100

    Add a test for object coercion

--- test/garm/spec_tools_test.clj
+++ test/garm/spec_tools_test.clj
@@ -1,8 +1,16 @@
 (ns garm.spec-tools-test
-  (:require [clojure.test :as t]
+  (:require [clojure.spec.alpha :as s]
+            [clojure.test :as t]
             [garm.specs :as specs]
             [spec-coerce.core :as sc]))
 
+(s/def ::a ::specs/int)
+(s/def ::obj
+  (s/keys :req-un [::a]))
+
 (t/deftest coerce-test
   (t/testing "should coerce data"
-    (t/is (= 1 (sc/coerce ::specs/int "1")))))
+    (t/is (= 1 (sc/coerce ::a "1"))))
+
+  (t/testing "should coerce an object"
+    (t/is (= {:a 1} (sc/coerce ::obj {:a "1"})))))
